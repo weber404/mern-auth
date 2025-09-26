58c9fb4c3426948bab7225e9e6c644e9a51cb5aa
Vishwas Saxena <185520121+weber404@users.noreply.github.com>
2025-09-26 22:35:37 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,9 +30,8 @@ export const register = async (req,res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ?
-            'none' : 'strict',
+            secure: true,
+            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -79,9 +78,8 @@ export const login = async (req,res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ?
-            'none' : 'strict',
+            secure: true,
+            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
